6452d71767a95cd5de8ce086508707a34c32ac5b
ohsang14 <ast6108@naver.com>
2024-04-10 18:27:47 +0900

    오타수정

--- src/mid1/study_01/immutable/test/ImmuableMyDateMain.java
+++ src/mid1/study_01/immutable/test/ImmuableMyDateMain.java
@@ -10,7 +10,10 @@ public class ImmuableMyDateMain {
 
         System.out.println("2025-> date1");
         date1 = date1.withYear(2025);
-        System.out.println("date1 = " + date1 );
-        System.out.println("date2 = " + date2 );
+        System.out.println("date1 = " + date1);
+        System.out.println("date2 = " + date2);
+
+
     }
+
 }
