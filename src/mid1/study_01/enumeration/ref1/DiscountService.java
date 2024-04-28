bc0c82bde138cb2c9c8d88a2a45ef14f00c87a3f
ohsang14 <ast6108@naver.com>
2024-04-28 15:08:21 +0900

    열거형 - 리팩토링

---
+++ src/mid1/study_01/enumeration/ref1/DiscountService.java
@@ -0,0 +1,9 @@
+package mid1.study_01.enumeration.ref1;
+
+
+public class DiscountService {
+    public int discount(ClassGrade classGrade, int price){
+
+        return price * classGrade.getDiscountPercent() / 100;
+    }
+}
