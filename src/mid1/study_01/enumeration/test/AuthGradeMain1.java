e19dbe4eac1cc73bde267351f19d16050198d40e
ohsang14 <ast6108@naver.com>
2024-05-04 14:54:38 +0900

    문제와 풀이 1

---
+++ src/mid1/study_01/enumeration/test/AuthGradeMain1.java
@@ -0,0 +1,13 @@
+package mid1.study_01.enumeration.test;
+
+public class AuthGradeMain1 {
+
+    public static void main(String[] args) {
+        AuthGrade[] values = AuthGrade.values();
+        for (AuthGrade value : values) {
+            System.out.println("grade=" + value.name() +
+                    ", level= " + value.getLevel() +
+                    ", 설명 = " + value.getDescription());
+        }
+    }
+}
